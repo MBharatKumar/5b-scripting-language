e36c4fc2d805b97fa621165b89a42d5baff6d71f
MBharatKumar <45728401+MBharatKumar@users.noreply.github.com>
2019-10-15 00:32:30 +0530

    Create 1c.py

---
+++ 1c.py
@@ -0,0 +1,13 @@
+#c) Read a list of numbers. Use a recursive function to find the maximum of “n” numbers. 
+
+def maxi(l2):
+    if len(l2) == 1:
+        return l2[0]
+    else:
+        return max(l2[0],maxi(l2[1:]))
+
+l1 = []
+for i in range(5):
+    l1.append(int(input()))
+
+print(maxi(l1))
