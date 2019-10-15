c0e2afbf5ce72b3f8f47ff61b672935f3ef1ff96
MBharatKumar <45728401+MBharatKumar@users.noreply.github.com>
2019-10-15 17:52:30 +0530

    Update 1c.py

--- 1c.py
+++ 1c.py
@@ -7,7 +7,9 @@ def maxi(l2):
         return max(l2[0],maxi(l2[1:]))
 
 l1 = []
-for i in range(5):
-    l1.append(int(input()))
+print("the program will print the maximum of n numbers")
+n = int(input("how many numbers you want to enter:  "))
+for i in range(n):
+    l1.append(int(input("enter the number")))
 
-print(maxi(l1))
+print("the highest number among the list is",maxi(l1))
